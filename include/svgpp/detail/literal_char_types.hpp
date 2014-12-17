8f43c10837fab202955703a77425ad7ca4aa0ae5
svgpp <olegmax@gmail.com>
2014-12-17 12:56:07 +0300

    Creating XML policy for Xerces parser

--- include/svgpp/detail/literal_char_types.hpp
+++ include/svgpp/detail/literal_char_types.hpp
@@ -9,11 +9,12 @@
 
 #include <boost/preprocessor/config/config.hpp>
 #include <boost/preprocessor/cat.hpp>
+#include <boost/preprocessor/facilities/empty.hpp>
 
 #ifndef SVGPP_CHAR_TYPES
 #  ifdef BOOST_NO_CXX11_UNICODE_LITERALS
-#    define SVGPP_CHAR_TYPES ((char, ""))((wchar_t, L))
+#    define SVGPP_CHAR_TYPES ((char, BOOST_PP_EMPTY()))((wchar_t, L))
 #  else
-#    define SVGPP_CHAR_TYPES ((char, ""))((wchar_t, L))((char16_t, u))((char32_t, U))
+#    define SVGPP_CHAR_TYPES ((char, BOOST_PP_EMPTY()))((wchar_t, L))((char16_t, u))((char32_t, U))
 #  endif
 #endif
